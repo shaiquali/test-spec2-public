5849d3717195511fa0fa433987e90ee8e125f16b
ShaiPi <88279994+shaiquali@users.noreply.github.com>
2023-03-06 16:34:42 +0200

    Update main.tf

--- terraform/azure-vm/main.tf
+++ terraform/azure-vm/main.tf
@@ -10,10 +10,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
-  client_id = var.client_id
-  client_secret = var.client_secret
 }
 
 ## <https://www.terraform.io/docs/providers/azurerm/r/resource_group.html>
