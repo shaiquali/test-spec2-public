90e670f8ccefc8b5b9c591dc581af6ec448b83fd
ShaiPi <88279994+shaiquali@users.noreply.github.com>
2023-03-29 14:26:02 +0300

    Update main.tf

--- terraform/ec2-simple/main.tf
+++ terraform/ec2-simple/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0f20f820a86c541d3"
+  ami           = "ami-00169914e6299b8e0"
   instance_type = "t2.micro"
 
   tags = {
