bb11fa8264d833e2b21e6c65b39e4eb3f49bced2
ShaiPi <88279994+shaiquali@users.noreply.github.com>
2023-08-09 08:59:35 +0300

    Create main.tf

---
+++ terraform/ec2-simple2/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">3.0.0"
+      # version = "~> 4.16"
+    }
+  }
+
+ #  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-west-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-00169914e6299b8e0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstancea"
+  }
+}
